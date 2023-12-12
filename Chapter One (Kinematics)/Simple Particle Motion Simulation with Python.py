fbdc1185c69a46b11efad489ea02b2c2c89fddd8
Mohsen Hafezghoran <153323582+Hafezghoran@users.noreply.github.com>
2023-12-12 17:27:58 +0330

    Add files via upload
    This Python script demonstrates the simulation of a particle's motion with constant acceleration in one dimension. Using the 'matplotlib' library for visualization and 'numpy' for computations, the program calculates the position and velocity of a particle over time based on initial conditions such as initial position, initial velocity, acceleration, and time duration. The resulting graph plots the particle's position and velocity as functions of time, providing a visual representation of the particle's motion.

---
+++ Chapter One (Kinematics)/Simple Particle Motion Simulation with Python.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def particle_motion(initial_position, initial_velocity, acceleration, time):
+    positions = []
+    velocities = []
+    for t in range(time):
+        position = initial_position + initial_velocity * t + 0.5 * acceleration * t**2
+        velocity = initial_velocity + acceleration * t
+        positions.append(position)
+        velocities.append(velocity)
+    return positions, velocities
+
+# Inputs
+initial_pos = 0
+initial_vel = 5
+acceleration = 2
+total_time = 10
+
+positions, velocities = particle_motion(initial_pos, initial_vel, acceleration, total_time)
+
+# Plotting
+time = np.arange(total_time)
+plt.figure(figsize=(8, 6))
+plt.subplot(2, 1, 1)
+plt.plot(time, positions, marker='o')
+plt.title('Particle Position over Time')
+plt.xlabel('Time')
+plt.ylabel('Position')
+
+plt.subplot(2, 1, 2)
+plt.plot(time, velocities, marker='o')
+plt.title('Particle Velocity over Time')
+plt.xlabel('Time')
+plt.ylabel('Velocity')
+
+plt.tight_layout()
+plt.show()
